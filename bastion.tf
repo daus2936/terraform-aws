427b90101b2ce14c59e876b86759a2adcd73a2c2
daus2936 <dausadinegoro@gmail.com>
2024-10-08 18:36:27 +0700

    new commit

--- bastion.tf
+++ bastion.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "vprofile-bastion" {
 
   provisioner "remote-exec" {
     inline = [
-      "chmod +x /tmp/vprofile-dbdeploy.sh",
+      "sudo chmod +x /tmp/vprofile-dbdeploy.sh",
       "sudo /tmp/vprofile-dbdeploy.sh"
     ]
   }
